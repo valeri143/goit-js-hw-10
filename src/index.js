6ed4b9712140d468a595477ac9598f9b48df8269
valeri143 <valeria23200523251605@gmail.com>
2023-04-20 22:03:01 +0300

    20.04

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import './css/styles.css';
+import { fetchCountries } from './fetchCounties';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+
+const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box')
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info')
+
+
+input.addEventListener('input', debounce(onInputQuery, DEBOUNCE_DELAY))
+function onInputQuery(){
+    const valueOfInputQuery = input.value.trim()
+if(valueOfInputQuery === ''){
+    clearCountryList();
+    return;
+}
+fetchCountries(valueOfInputQuery)
+.then(countries => {
+    if (countries.length > 10) {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      clearCountryList();
+    } else if (countries.length >= 2 && countries.length < 10){
+        const markup = countries.map(country => `
+      <li>
+        <img src="${country.flags.svg}" alt="${country.name}" width="32">
+        <span>${country.name}</span>
+      </li>
+    `).join('');
+    countryList.innerHTML = markup;
+    } else if(countries.length === 1){
+        clearCountryList()
+const markupForOneCountry = countries.map(country =>{
+    `<img src="${country.flags.svg}" alt="${country.name}" width="32">
+<h2>${country.name}</h2>
+<ul>
+  <li><h3>Capital:</h3><span>${country.capital}</span></li>
+  <li><h3>Population:</h3><span>${country.population}</span></li>
+  <li><h3>Languages:</h3><span>${country.languages}</span></li>
+</ul>`.join('')
+})
+countryInfo.innerHTML = markupForOneCountry;
+    }
+})
+.catch(error => {
+if(error === '404'){
+Notiflix.Notify.failure("Oops, there is no country with that name");}
+})
+}
+function clearCountryList() {
+    countryList.innerHTML = '';
+  }
